c8e9402d0072ec93bfe389af5dd3e0184487295c
shiv401 <73016782+shiv401@users.noreply.github.com>
2022-11-20 17:45:47 +0530

    Add files via upload

---
+++ QuestionAnswer2.java
@@ -0,0 +1,48 @@
+package com.shiv.easyquizapp;
+
+import java.util.Locale;
+
+public class QuestionAnswer2 {
+
+    public static String question[] ={
+            "Q.1) Giddha is the folk dance of?",
+            "Q.2) Who was the first Prime Minister of India?",
+            "Q.3) Who invented Computer?",
+            "Q.4) 1024 Kilobytes is equal to?",
+            "Q.5) Brain of computer is?",
+            "Q.6) India lies in which continent?",
+            "Q.7) What city is the statue of liberty in?",
+            "Q.8) Environment Day is celebrated every year on?",
+            "Q.9) Biggest mammal on Earth is?",
+            "Q.10) Which is the longest river in India?"
+    };
+
+    public static String choices[][] = {
+            {"Gujrat","Punjab","Maharastra","Rajasthan"},
+            {"Narendraa Modi","Jawaharlal Nehru","Rajiv Gandhi","Soniya Gandhi"},
+            {"Mark Zukerberg","Anthony Babbage","Charles Babbage","Sunderlal Panchali"},
+            {"1 MB","1 KB","10 KB","10 MB"},
+            {"RAM(Rnadom Access Memory)","CU(Control Unit)","ALU(Arithmetic Logical Unit)","CPU(Central Processing Unit)"},
+            {"Antartica","Europe","Australia","Asia"},
+            {"Canada","New York","Paris","London"},
+            {"5th July","21st July","5th June","21st June"},
+            {"Elephant","Shark","Blue Whale","OStrich"},
+            {"Gomti","Yamuna","Godaveri","Ganga"}
+    };
+
+    public static String correctAnswers[] = {
+            "Punjab",
+            "Jawaharlal Nehru",
+            "Charles Babbage",
+            "1 MB",
+            "CPU(Central Processing Unit)",
+            "Asia",
+            "New York",
+            "5th June",
+            "Blue Whale",
+            "Ganga"
+
+    };
+
+
+}
---
+++ QuestionAnswer3.java
@@ -0,0 +1,47 @@
+package com.shiv.easyquizapp;
+
+import java.util.Locale;
+
+public class QuestionAnswer3 {
+
+    public static String question[] ={
+            "Q.1) What is the full form of RAM in terms of Computer?",
+            "Q.2) In which state does the Pichola waterfall(jheel) is situated?",
+            "Q.3) Which mammal has more than one Heart?",
+            "Q.4) What is name of the first English Newspaper of India?",
+            "Q.5) Which place has more difference in the temperature of day and night?",
+            "Q.6) Name of the president of India who take oath on 25 July 2022?",
+            "Q.7) Name of the God who is mainly worshipped in 'Shravan(सावन)?",
+            "Q.8) Which place is known as pink city?",
+            "Q.9) Name of the National flag of India?",
+            "Q.10) Choose Odd one out from the following:"
+    };
+
+    public static String choices[][] = {
+            {"Ring Associative Memory","Random Access Memory","Ring Associative Machine","Regular Access Machine"},
+            {"Uttar Pradesh","Gujrat","Rajasthan","Madhya Pradesh"},
+            {"Blue Whale","Octopus","Starfish","Ostrich"},
+            {"Bengal Gajat","Indian Express","Times Of India","Foundation Express"},
+            {"Sea","Cave","Mountains","Desert"},
+            {"Ram Nath Kovind","Dr.Rajendra Prasad","Droupadi Murmu","Pranab Mukherji"},
+            {"Lord Indradev","Lord Krishna","Lord Ganesha","Lord Shiva"},
+            {"New York City","Raipur","Jaipur","Paris"},
+            {"Bhagwa","Tiranga","Firnagi","Satyamev Jayate"},
+            {"Pushpa","Babool","Tulsi","Neem"}
+    };
+
+    public static String correctAnswers[] = {
+            "Random Access Memory",
+            "Rajasthan",
+            "Octopus",
+            "Bengal Gajat",
+            "Desert",
+            "Droupadi Murmu",
+            "Lord Shiva",
+            "Jaipur",
+            "Tiranga",
+            "Pushpa"
+    };
+
+
+}
